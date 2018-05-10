4cc3ae09d4844b8b7781f44f2adf4ebccf0bff18
KingBoyBIT <405613185@qq.com>
2018-05-10 22:15:14 +0800

    learningcodes

--- TensorFlowLearningCodes/mnist_bp.py
+++ TensorFlowLearningCodes/mnist_bp.py
@@ -1,32 +1,36 @@
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
-print(mnist.train.images.shape, mnist.train.labels.shape)
-print(mnist.test.images.shape, mnist.test.labels.shape)
-print(mnist.validation.images.shape, mnist.validation.labels.shape)
-
 import tensorflow as tf
-sess = tf.InteractiveSession()
-x = tf.placeholder(tf.float32, [None, 784])
+import numpy as np
 
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
 
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+def addLayer(inputData, inSize, outSize, activity_function=None):
+    Weights = tf.Variable(tf.random_normal([inSize, outSize]))
+    basis = tf.Variable(tf.zeros([1, outSize]) + 0.1)
+    weights_plus_b = tf.matmul(inputData, Weights) + basis
+    if activity_function is None:
+        ans = weights_plus_b
+    else:
+        ans = activity_function(weights_plus_b)
+    return ans
 
-y_ = tf.placeholder(tf.float32, [None, 10])
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]  # 转为列向量
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) + 0.5 + noise
 
-tf.global_variables_initializer().run()
+xs = tf.placeholder(tf.float32, [None, 1])  # 样本数未知，特征数为1，占位符最后要以字典形式在运行中填入
+ys = tf.placeholder(tf.float32, [None, 1])
 
-for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    train_step.run({x: batch_xs, y_: batch_ys})
+l1 = addLayer(xs, 1, 10, activity_function=tf.nn.relu)  # relu是激励函数的一种
+l2 = addLayer(l1, 10, 1, activity_function=None)
+loss = tf.reduce_mean(tf.reduce_sum(tf.square((ys - l2)), reduction_indices=[1]))  # 需要向相加索引号，redeuc执行跨纬度操作
 
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)  # 选择梯度下降法
 
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
 
-print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
+for i in range(10000):
+    sess.run(train, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        print sess.run(loss, feed_dict={xs: x_data, ys: y_data})
